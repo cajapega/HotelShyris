85bbfbb845238a5dd5e01968e5965a0729573a1c
Carlos Perez <charlyjun@gmail.com>
2022-11-06 12:36:56 -0500

    Doy formato al boton Menu

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
 const navToggle = document.querySelector(".nav-toggle");
 const navMenu = document.querySelector(".nav-menu");
+const body = document.querySelector(".body")
+
 
 navToggle.addEventListener("click", () => {
     navMenu.classList.toggle("nav-menu_visible");
+})
+
+body.addEventListener("Click", () => {
+    navMenu.classList.toggle("nav-menu_visible")
 })
