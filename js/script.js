e3b37dad67982a02bdce7d6cc9e777799869f954
Carlos Perez <charlyjun@gmail.com>
2022-11-06 18:40:11 -0500

    Se elimina la seccion IMG y se la adjnta a la seccion rooms las imagenes estan cargadas pero, hay que revisar bien los selectores que se estan usando ya que eso da error al momento de dar padding en los div y tambien al desactivar el boton Ver de rooms

--- js/script.js
+++ js/script.js
@@ -1,9 +1,15 @@
+//Funcionalidad boton Menu
 const navToggle = document.querySelector(".nav-toggle");
 const navMenu = document.querySelector(".nav-menu");
-const body = document.querySelector(".body")
-
 
 navToggle.addEventListener("click", () => {
     navMenu.classList.toggle("nav-menu_visible");
 })
 
+//Funcionlidad Boton Ver imagenes
+//const buttonRooms = document.querySelector(".button-rooms");
+//const containerImg  = document.querySelector(".container-img");
+
+/*buttonRooms.addEventListener("Click", () => {
+    containerImg.classList.toggle("container-img_visible");
+})*/
