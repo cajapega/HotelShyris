fb08baffb98c8556d91aebb080c93edfba54702a
Carlos Perez <charlyjun@gmail.com>
2022-11-09 22:08:38 -0500

    dejo funcional el boton img

--- js/script.js
+++ js/script.js
@@ -1,15 +1,22 @@
-//Funcionalidad boton Menu
+/*Antigua Funcionalidad boton Menu
 const navToggle = document.querySelector(".nav-toggle");
 const navMenu = document.querySelector(".nav-menu");
 
 navToggle.addEventListener("click", () => {
     navMenu.classList.toggle("nav-menu_visible");
-})
+})*/
+function menu
+
+
 
 //Funcionlidad Boton Ver imagenes
-const buttonRooms = document.querySelector(".button-rooms");
-const containerImg  = document.querySelector(".container-img");
-
-buttonRooms.addEventListener("click", () => {
-    containerImg.classList.toggle("container-img_visible");
-})
+function mostrar() {
+    let containerImg = document.getElementById("container-img");
+    let containerImgStyle = getComputedStyle(containerImg);
+    let cidisplay = containerImgStyle.getPropertyValue("display");
+    if (cidisplay == "none") {
+        document.getElementById("container-img").style.display = "flex";
+    } if (cidisplay == "flex" ) {
+        document.getElementById("container-img").style.display = "none";
+    }
+}
